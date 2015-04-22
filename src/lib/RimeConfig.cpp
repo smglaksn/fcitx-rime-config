241c53570445a2128c58f78e4148df337cc67c8d
Xu Zhao <xuzhao@dlrz.eecg>
2015-04-22 16:00:17 -0400

    Resume dev

---
+++ src/lib/RimeConfig.cpp
@@ -0,0 +1,5 @@
+//
+// Created by xuzhao on 4/22/15.
+//
+
+#include "RimeAPI.h"
---
+++ src/lib/RimeConfig.h
@@ -0,0 +1,15 @@
+//
+// Author: Xu Zhao (i@xuzhao.net)
+//
+
+#ifndef FCITX_RIME_CONFIG_RIMEAPI_H
+#define FCITX_RIME_CONFIG_RIMEAPI_H
+
+#include <string>
+using namespace std;
+
+class RimeConfig {
+    string m_conf_path;
+};
+
+#endif //FCITX_RIME_CONFIG_RIMEAPI_H
